251ef709a51a8308a1a006498cf1d5e1f0457ff6
Michael J. Rasicci <mike@mjrasicci.dev>
2025-10-19 22:02:19 -0400

    Refactor editor to use service host (#9)

--- Editor/source/Editor.cpp
+++ Editor/source/Editor.cpp
@@ -1,19 +1,57 @@
 #include "Editor.hpp"
 
-Editor::Editor()
+#include <stdexcept>
+
+#include "Services/ServiceCollection.hpp"
+#include "Services/ServiceLocator.hpp"
+#include "Services/ServiceProvider.hpp"
+#include "Services/ServiceTraits.hpp"
+
+TGE_DECLARE_SERVICE_DEPENDENCIES(Editor, TGE::InjectLocator());
+
+Editor::Editor(TGE::ServiceLocator& locator)
 {
-    TGE::LoggingOptions options;
-    globalLogger = std::make_shared<TGE::GlobalLogger>(options);
-    logger = std::make_shared<TGE::Logger<Editor>>(globalLogger);
+    auto* providerLocator = dynamic_cast<TGE::ServiceProvider*>(&locator);
+
+    if (!providerLocator)
+    {
+        throw std::invalid_argument("Editor requires a ServiceProvider-backed locator.");
+    }
+
+    provider = providerLocator->shared_from_this();
 }
 
 void Editor::Run()
 {
+    Start();
+}
+
+void Editor::ConfigureServices(TGE::ServiceCollection& services)
+{
+    services.AddTransient<TGE::Logger<Editor>>();
+}
+
+void Editor::OnStart()
+{
+    globalLogger = provider->GetRequiredService<TGE::GlobalLogger>();
+    logger = provider->GetRequiredService<TGE::Logger<Editor>>();
     logger->Info("Starting Editor...");
+}
+
+void Editor::OnStop()
+{
+    if (globalLogger)
+    {
+        globalLogger->Flush();
+    }
 }
 
 int main()
 {
-    Editor editor;
-    editor.Run();
+    TGE::ServiceCollection services;
+    services.AddTransient<Editor>();
+
+    auto provider = services.BuildServiceProvider();
+    auto editor = provider->GetRequiredService<Editor>();
+    editor->Run();
 }
